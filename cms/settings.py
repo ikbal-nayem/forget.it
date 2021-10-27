854d32d10d902be686b26f7584d68626e9b346b2
Ikbal Nayem <ikbalnayem000@gmail.com>
2021-10-27 16:49:06 +0600

    cors policy allowed to all

--- cms/settings.py
+++ cms/settings.py
@@ -52,10 +52,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://*:3000",
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'cms.urls'
 
