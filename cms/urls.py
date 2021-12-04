9bda6ceef005cd2e6a656994332f34cbc7cdc726
Ikbal Nayem <ikbalnayem000@gmail.com>
2021-12-04 16:37:20 +0600

    front change

--- cms/urls.py
+++ cms/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.shortcuts import redirect
+
+
+
+def redirectToFront(request):
+    return redirect("https://forgetit.glitch.me")
 
 urlpatterns = [
     path('super-admin/', admin.site.urls),
     path('api/user/', include('users.urls')),
     path('api/credentials/', include('credentials.urls')),
+    path('', redirectToFront)
 ]
